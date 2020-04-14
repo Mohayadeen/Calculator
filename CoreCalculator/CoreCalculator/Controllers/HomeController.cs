5ea0ff7001ce9383ddff8f916a6b2ff31f2014e7
MABDULK8 <mabdulk8@ford.com>
2020-04-14 16:22:27 +0530

    Calculator V1

---
+++ CoreCalculator/CoreCalculator/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using CoreCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Operation model)
+        {
+            if (model.OperationType == OperationType.Addition)
+                model.Result = model.NumberA + model.NumberB;
+            else if (model.OperationType == OperationType.Subtraction)
+                model.Result = model.NumberA - model.NumberB;
+            else if (model.OperationType == OperationType.Multiplication)
+                model.Result = model.NumberA * model.NumberB;
+            else model.Result = model.NumberA / model.NumberB;
+            
+            return View(model);
+        }
+    }
+}
